03a52a57227cfe23b5d033832bf3fd72b5a7e1e2
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-10-28 15:28:59 -0500

    Create, edit & complete Tasks (no persistence)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,84 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, fromEvent, tap } from 'rxjs';
+import { Task } from 'src/app/models/task.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  tasks: Task[] = [];
+  newTask: string = '';
 
-  constructor() { }
+  isEditingActive: boolean = false;
+  editingTaskId: string | null = null;
+  counter: number = 0;
+  editingTask: string = '';
+
+  submitObs$ = fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+    filter((e) => e.code === 'Enter' || e.code === 'Escape')
+  );
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.submitSubscription();
   }
 
+  submitSubscription(): void {
+    this.submitObs$.subscribe((e) => {
+      if (this.isEditingActive) {
+        this.editTask(e);
+      }
+      if (e.code === 'Enter' && this.newTask.trim().length > 0) {
+        this.createTask();
+      }
+    });
+  }
+
+  createTask() {
+    this.tasks.push({
+      id: this.counter.toString(),
+      title: this.newTask,
+      completed: false,
+      isEditing: false,
+    });
+    this.newTask = '';
+    this.counter++;
+  }
+
+  toggleTaskStatus(taskId: string): void {
+    this.tasks = this.tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+  }
+
+  openEditMode(taskId: string): void {
+    this.tasks = this.tasks.map((task) => {
+      if (task.id === taskId) {
+        this.editingTask = task.title;
+        this.editingTaskId = task.id;
+        return { ...task, isEditing: true };
+      }
+      return { ...task, isEditing: false };
+    });
+    this.isEditingActive = true;
+  }
+
+  editTask(e: KeyboardEvent) {
+    if (e.code === 'Enter') {
+      this.tasks = this.tasks.map((task) =>
+        task.id === this.editingTaskId
+          ? { ...task, title: this.editingTask }
+          : task
+      );
+    }
+    this.closeEditMode();
+  }
+
+  closeEditMode(): void {
+    this.tasks = this.tasks.map((task) => ({ ...task, isEditing: false }));
+    this.isEditingActive = false;
+    this.editingTaskId = null;
+  }
 }
